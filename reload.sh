aa73aec1b5c102a88e54be9822efc7bd326a97d5
opencan <jgonzalez@opencanarias.es>
2015-12-11 14:02:38 +0000

    fichero reload

---
+++ reload.sh
@@ -0,0 +1,2 @@
+docker inspect --format '{{.Name}} {{.NetworkSettings.IPAddress}} check' $(docker ps | grep libreoffice | cut -f 1,3 -d ' ') | sed -r -e 's/\//  server /g' > /tmp/servers.txt
+cat /etc/haproxy/haproxy_default.cfg /tmp/servers.txt > /etc/haproxy/haproxy.cfg
